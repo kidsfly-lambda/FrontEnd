c900b2077fef123c6b61a3e64397d380a04652e7
Louis Magdaleno <buildthatapp@gmail.com>
2019-04-30 23:24:29 -0700

    Added presentation components

--- kidsfly/src/components/Flight.js
+++ kidsfly/src/components/Flight.js
@@ -1,1 +1,10 @@
 // dumb component for displaying a flight
+import React from 'react';
+
+const Flight = props => {
+    return (
+        <h2>From : {props.origin} to {props.destination}</h2>
+    )
+}
+
+export default Flight;
